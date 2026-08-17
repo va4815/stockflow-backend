3a95a2a38db603026493a6a46761e23f490bc048
va4815 <va543567@gmail.com>
2026-08-17 11:23:23 +0100

    initial project

---
+++ stockflow-backend/src/main/java/com/va4815/stockflow/StockflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.va4815.stockflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockflowBackendApplication.class, args);
+	}
+
+}
---
+++ stockflow-backend/src/test/java/com/va4815/stockflow/StockflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.va4815.stockflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
